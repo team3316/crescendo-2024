84c6657b94be890fe011c347d8fcd2975fdace9f
UriAvidan <aavidan.uri@gmail.com>
2024-01-15 20:23:50 +0200

    added setState and PID

--- src/main/java/frc/robot/constants/constantsShooter.java
+++ src/main/java/frc/robot/constants/constantsShooter.java
@@ -1,7 +1,10 @@
 package frc.robot.constants;
 
 public class constantsShooter {
-    public static int SparkMaxRightPort;
-    public static int SpatkMaxLeftPort;
-    public static int SparkMaxShootingPercent;
+    public static final int SparkFlexRightPort = 0;
+    public static final int SparkFlexLeftPort = 0;
+    public static final int SparFlexShootingVelocity = 0; //rpm
+    public static final int SparkFlexUnShootingVelocity = 0;//rpm
+    public static final double kpShooter = 1;
+    public static final double kvFeedForwardShooter = 0; // v/rpm
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,32 +1,74 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.constantsShooter;
+import frc.robot.motors.DBugSparkFlex;
 import frc.robot.motors.DBugSparkMax;
+import frc.robot.motors.PIDFGains;
+
+import com.revrobotics.SparkFlexExternalEncoder;
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.SparkPIDController.ArbFFUnits;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends SubsystemBase{
-    private DBugSparkMax _SparkMaxRightLeader;
-    private DBugSparkMax _SparkMaxLeftFollower;
+    private DBugSparkFlex _SparkFlexRightLeader;
+    private DBugSparkFlex _SparkFlexLeftFollower;
+    private ShooterState _ShooterState;
+
+    private SimpleMotorFeedforward feedForward;
+    
 
     public static enum ShooterState{
-        ON(constantsShooter.SparkMaxShootingPercent),
-        OFF(0);
-        public final double percentage;
+        ON(constantsShooter.SparFlexShootingVelocity),
+        OFF(constantsShooter.SparkFlexUnShootingVelocity);
+        public final double v;
 
-        private ShooterState(double percentage){
-            this.percentage = percentage;
+        private ShooterState(double v){
+            this.v = v;
         }
     }
 
     public Shooter(){
         
-        _SparkMaxRightLeader = new DBugSparkMax(constantsShooter.SparkMaxRightPort);
-        _SparkMaxLeftFollower = new DBugSparkMax(constantsShooter.SpatkMaxLeftPort);
+        _SparkFlexRightLeader = DBugSparkFlex.create(constantsShooter.SparkFlexRightPort);
+        _SparkFlexLeftFollower = DBugSparkFlex.create(constantsShooter.SparkFlexLeftPort);
 
-        _SparkMaxLeftFollower.follow(_SparkMaxRightLeader);
+        _SparkFlexLeftFollower.setupPIDF(new PIDFGains(constantsShooter.kpShooter));
+        
 
+        _SparkFlexLeftFollower.follow(_SparkFlexRightLeader, true);
+
+        feedForward = new SimpleMotorFeedforward(0, constantsShooter.kvFeedForwardShooter);
 
 
     }
+
+    public ShooterState getShooterState(){
+        return this._ShooterState;
+    }
+
+    public Command getSetStateCommand(ShooterState shooterState){
+        return new InstantCommand(() -> setState(shooterState), this);
+
+        
+    }
+
+    private void setState(ShooterState shooterState){
+        _ShooterState = shooterState;
+        _SparkFlexRightLeader.setReference(shooterState.v, ControlType.kVelocity, 0, feedForward.calculate(shooterState.v), ArbFFUnits.kVoltage);
+        SmartDashboard.putString("State:",_ShooterState.toString());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("velocity, rpm", _SparkFlexRightLeader.getVelocity());
+
+    }
+
 }
