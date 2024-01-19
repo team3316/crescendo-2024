7057df8adc59eae14e16b1b032b47f7fbd181738
gurilad <giladbh21@gmail.com>
2024-01-19 12:26:59 +0200

    Added get and set state

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,5 +1,8 @@
 package frc.robot.constants;
 
 public class IntakeConstants {
-    
+    public final static int intakeMotorID = 1;
+
+    public final static double collectingPrecent = 0.4;
+    public final static double disabledPrecent = 0;
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,13 +1,38 @@
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IntakeConstants;
 import frc.robot.motors.DBugSparkMax;
 
-public class Intake extends SubsystemBase{
-    private DBugSparkMax _leader;
-    private DBugSparkMax _follower;
+public class Intake extends SubsystemBase {
+    private DBugSparkMax _intakeMotor;
 
-    public Intake() {
-        
+    private CollectorState _state;
+
+    public enum CollectorState {
+        COLLECTING(IntakeConstants.collectingPrecent),
+        DISABLED(IntakeConstants.disabledPrecent);
+        public final double precentege;
+
+        private CollectorState(double precentege){
+            this.precentege = precentege;
+        }
+    }
+
+    public Intake(){
+        _intakeMotor = new DBugSparkMax(IntakeConstants.intakeMotorID);
+
+        this._state = CollectorState.DISABLED; 
+    }
+
+    public CollectorState getState(){
+        return this._state;
+    }
+
+    public void setState(CollectorState state){
+        this._state = state;
+
+        _intakeMotor.setReference(state.precentege, ControlType.kVelocity);
     }
 }
