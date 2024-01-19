f2eb515a4648313f34a514f9cec10bfd1ef395cb
UriAvidan <aavidan.uri@gmail.com>
2024-01-19 14:48:42 +0200

    added kp and velocity calibration

--- src/main/java/frc/robot/constants/constantsShooter.java
+++ src/main/java/frc/robot/constants/constantsShooter.java
@@ -3,8 +3,8 @@ package frc.robot.constants;
 public class constantsShooter {
     public static final int SparkFlexRightPort = 0;
     public static final int SparkFlexLeftPort = 0;
-    public static final int SparFlexShootingVelocity = 0; //rpm
+
     public static final int SparkFlexUnShootingVelocity = 0;//rpm
-    public static final double kpShooter = 1;
+
     public static final double kfShooter = 0.00178571428; // (1/560)v/rpm 
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -16,74 +16,78 @@ import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Shooter extends SubsystemBase{
-    private DBugSparkFlex _SparkFlexRightLeader;
-    private DBugSparkFlex _SparkFlexLeftFollower;
-    private ShooterState _ShooterState;
+public class Shooter extends SubsystemBase {
+    private DBugSparkFlex _sparkFlexRightLeader;
+    private DBugSparkFlex _sparkFlexLeftFollower;
+    private ShooterState _shooterState;
 
+    private static double SparFlexShootingVelocity; // rpm
+
+    private static double kpShooter = 1;
 
     
 
-    public static enum ShooterState{
-        ON(constantsShooter.SparFlexShootingVelocity),
+    public static enum ShooterState {
+        ON(SparFlexShootingVelocity),
         OFF(constantsShooter.SparkFlexUnShootingVelocity);
         public final double velocity;
 
-        private ShooterState(double velocity){
+        private ShooterState(double velocity) {
             this.velocity = velocity;
         }
     }
 
-    public Shooter(){
+    public Shooter() {
         
-        _SparkFlexRightLeader = DBugSparkFlex.create(constantsShooter.SparkFlexRightPort);
-        _SparkFlexLeftFollower = DBugSparkFlex.create(constantsShooter.SparkFlexLeftPort);
+        this._sparkFlexRightLeader = DBugSparkFlex.create(constantsShooter.SparkFlexRightPort);
+        this._sparkFlexLeftFollower = DBugSparkFlex.create(constantsShooter.SparkFlexLeftPort);
 
-        _SparkFlexLeftFollower.setupPIDF(new PIDFGains(constantsShooter.kpShooter,0,0,constantsShooter.kfShooter));
+        this._sparkFlexLeftFollower.setupPIDF(new PIDFGains(kpShooter, 0, 0, constantsShooter.kfShooter));
         
 
-        _SparkFlexLeftFollower.follow(_SparkFlexRightLeader, true);
+        this._sparkFlexLeftFollower.follow(_sparkFlexRightLeader, true);
 
-        //sets the shooter state to OFF in the beginning
-        this._ShooterState = ShooterState.OFF;
+        // sets the shooter state to OFF in the beginning
+        this._shooterState = ShooterState.OFF;
         SmartDashboard.putString("State:", "OFF, enabled");
-
-
     }
 
-    public ShooterState getShooterState(){
-        return this._ShooterState;
+    public ShooterState getShooterState() {
+        return this._shooterState;
     }
 
-    public Command getSetStateCommand(ShooterState shooterState){
-        //only sets the Shooter's state, doesn't stops shooting independently
-        return new InstantCommand(() -> setState(shooterState), this); 
+    public Command getSetStateCommand(ShooterState shooterState) {
+        // only sets the Shooter's state, doesn't stops shooting independently
+        return new InstantCommand(() -> setState(shooterState), this);
     }
 
+    private void setState(ShooterState shooterState) {
+        this._shooterState = shooterState;
+        this._sparkFlexRightLeader.setReference(shooterState.velocity, ControlType.kVelocity);
 
-
-    private void setState(ShooterState shooterState){
-        _ShooterState = shooterState;
-        _SparkFlexRightLeader.setReference(shooterState.velocity, ControlType.kVelocity);
-
-        SmartDashboard.putString("State:",_ShooterState.toString());
-        //prints the state change ont the SmartDashboard
+        SmartDashboard.putString("State:", this._shooterState.toString());
+        // prints the state change ont the SmartDashboard
     }
 
     @Override
     public void periodic() {
-        //prints the velocity to the SmartDashboard
-        SmartDashboard.putNumber("velocity, rpm", _SparkFlexRightLeader.getVelocity());
+        // prints the velocity to the SmartDashboard
+        SmartDashboard.putNumber("velocity, rpm", this._sparkFlexRightLeader.getVelocity());
 
+        // gets the velocity value from the SmartDashboard
+        SparFlexShootingVelocity = SmartDashboard.getNumber("velocity", 0);
+
+        // gets the kp value from the SmartDashboard
+        kpShooter = SmartDashboard.getNumber("kp", 1);
     }
 
-    //runs when is disable
-    public void disableInit(){
-        this._ShooterState = ShooterState.OFF;
-        _SparkFlexRightLeader.setReference(_ShooterState.velocity, ControlType.kVelocity);
+    // runs when is disable
+    public void disableInit() {
+        this._shooterState = ShooterState.OFF;
+        this._sparkFlexRightLeader.setReference(this._shooterState.velocity, ControlType.kVelocity);
 
-        SmartDashboard.putString("State:",_ShooterState.toString() + ", disabled");
-        //prints the state change ont the SmartDashboard
+        // prints the state change ont the SmartDashboard
+        SmartDashboard.putString("State:", this._shooterState.toString() + ", disabled");
     }
 
 }
