8dae10b1168eb3bb24f36b7d9a489b83b5bfb438
--global <aavidan.uri@gmail.com>
2024-01-19 11:31:02 +0200

    added motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.motors.DBugSparkMax;
 
 public class Intake extends SubsystemBase{
-    
+    private DBugSparkMax _leader;
+    private DBugSparkMax _follower;
 }
