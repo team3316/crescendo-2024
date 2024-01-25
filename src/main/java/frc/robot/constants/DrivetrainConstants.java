4d77a0b9570717a3ad978e24f130bad29caf2f17
yoavpeles <yoav.peles@gmail.com>
2024-01-25 19:11:23 +0200

    configed the cancoder offset

--- src/main/java/frc/robot/constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -67,10 +67,10 @@ public class DrivetrainConstants {
     public static final double frontWheelDistMeters = 0.6703;
     public static final double sideWheelDistMeters = 0.5102;
 
-    public final static double cancoderTLOffset = 33.2;
-    public final static double cancoderTROffset = 324.0;
-    public final static double cancoderBLOffset = 182.8;
-    public final static double cancoderBROffset = 110.4;
+        public final static double cancoderTROffset = 299.2;
+        public final static double cancoderTLOffset = 90.8;
+        public final static double cancoderBROffset = 285.2;
+        public final static double cancoderBLOffset = 326.5;
     public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(
             new Translation2d(sideWheelDistMeters / 2, frontWheelDistMeters / 2), 14,3,
             cancoderTLOffset, 12);
