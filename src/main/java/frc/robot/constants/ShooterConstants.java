5146f9a8f62202b124393ae88292cbb14238e208
UriAvidan <aavidan.uri@gmail.com>
2024-01-26 17:01:37 +0200

    added motors, removed switches and updated constants

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -3,11 +3,9 @@ package frc.robot.constants;
 public class ShooterConstants {
     public static final int SparkFlexRightPort = 0;
     public static final int SparkFlexLeftPort = 0;
-    public static final int ShooterRollerPort = 0;
 
     public static final double SparkFlexUnShootingVelocity = 0; // rpm
     public static final double SparkFlexShootingVelocity = 1; // rpm
-    public static final double ShooterRollerPercent = 0; // percent
 
     public static final double kpShooter = 1; // v/rpm
     public static final double kfShooter = 0.00178571428; // (1/560)v/rpm 
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,28 +1,23 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.constants.ShooterConstants;
 import frc.robot.motors.DBugSparkFlex;
 import frc.robot.motors.PIDFGains;
 import frc.robot.utils.Within;
 
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.SparkLimitSwitch.Type;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends SubsystemBase {
 
-    private DBugSparkFlex _leader;
-    private DBugSparkFlex _follower;
+    private DBugSparkFlex _leaderUpLeft;
+    private DBugSparkFlex _followerUpRight;
+    private DBugSparkFlex _followerDownLeft;
+    private DBugSparkFlex _followerDownRight;
     private ShooterState _shooterState;
 
     public static enum ShooterState {
@@ -38,12 +33,16 @@ public class Shooter extends SubsystemBase {
 
     public Shooter() {
 
-        this._leader = DBugSparkFlex.create(ShooterConstants.SparkFlexRightPort);
-        this._follower = DBugSparkFlex.create(ShooterConstants.SparkFlexLeftPort);
+        this._leaderUpLeft = DBugSparkFlex.create(ShooterConstants.SparkFlexRightPort);
+        this._followerUpRight = DBugSparkFlex.create(ShooterConstants.SparkFlexLeftPort);
+        this._followerDownLeft = DBugSparkFlex.create(ShooterConstants.SparkFlexLeftPort);
+        this._followerDownRight = DBugSparkFlex.create(ShooterConstants.SparkFlexLeftPort);
 
-        this._leader.setupPIDF(new PIDFGains(ShooterConstants.kpShooter, 0, 0, ShooterConstants.kfShooter));
+        this._leaderUpLeft.setupPIDF(new PIDFGains(ShooterConstants.kpShooter, 0, 0, ShooterConstants.kfShooter));
 
-        this._follower.follow(_leader, true);
+        this._followerUpRight.follow(_leaderUpLeft, true);
+        this._followerDownLeft.follow(_leaderUpLeft, false);
+        this._followerDownRight.follow(_leaderUpLeft, true);
 
         // sets the shooter state and the roller state to OFF in the beginning
         this._shooterState = ShooterState.OFF;
@@ -55,18 +54,9 @@ public class Shooter extends SubsystemBase {
         return this._shooterState;
     }
 
-    // TODO: check which is forward and which reverse, and whetehr they are NC or NO
-    public boolean getSlowingLiftSwitch() {
-        return _leader.getReverseLimitSwitch(Type.kNormallyOpen).isPressed();
-    }
-
-    public boolean getStoppingLiftSwitch() {
-        return _leader.getForwardLimitSwitch(Type.kNormallyOpen).isPressed();
-    }
-
     private void setState(ShooterState shooterState) {
         this._shooterState = shooterState;
-        this._leader.setReference(shooterState.velocity, ControlType.kVelocity);
+        this._leaderUpLeft.setReference(shooterState.velocity, ControlType.kVelocity);
 
         // prints the state change onto the SmartDashboard
         SmartDashboard.putString("shooter state:", this._shooterState.toString());
@@ -77,7 +67,7 @@ public class Shooter extends SubsystemBase {
     }
 
     public double getMotorVelocity() {
-        return this._leader.getVelocity();
+        return this._leaderUpLeft.getVelocity();
     }
 
     public boolean isAtTargetVelocity() {
@@ -87,9 +77,8 @@ public class Shooter extends SubsystemBase {
     @Override
     public void periodic() {
         /* gets the velocity value from the SmartDashboard. To use these lines for calibration remove final keyword from 
-        ShooterState.velocity & RollerState.ShooterRollerPercent and remember to put it back*/
+        ShooterState.velocity and remember to put it back*/
         // ShooterState.ON.velocity = SmartDashboard.getNumber("velocity, rpm", 0);
-        // RollerState.ON.ShooterRollerPercent = SmartDashboard.getNumber("Roller velocity, percentage", 0);
         SmartDashboard.putNumber("Shooter velocity", getMotorVelocity());
         /*double curKP = SmartDashboard.getNumber("kp", 1);
         // only updates when the values are changed
