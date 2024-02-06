880a65f808deb44848a6bfdeac5c72c6bd340c26
itamrit77 <itamrit77@gmail.com>
2024-02-06 13:06:18 +0200

    commented other subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,13 +42,13 @@ import frc.robot.subsystems.drivetrain.SwerveSysidCommands;
 public class RobotContainer {
 
     private final Drivetrain m_Drivetrain = new Drivetrain();
-    private final Arm m_Arm = new Arm();
+    /*private final Arm m_Arm = new Arm();
     private final Manipulator m_Manipulator = new Manipulator();
     private final Shooter m_Shooter = new Shooter();
     private final Intake m_Intake = new Intake();
     private final Climber m_Climber = new Climber(() -> Rotation2d.fromDegrees(m_Drivetrain.getRoll()));
 
-    private final CommandPS5Controller m_buttonController = new CommandPS5Controller(JoysticksConstants.operatorPort);
+    private final CommandPS5Controller m_buttonController = new CommandPS5Controller(JoysticksConstants.operatorPort);*/
     private final CommandPS5Controller _driverController = new CommandPS5Controller(JoysticksConstants.driverPort);
 
     private boolean _fieldRelative = true;
@@ -72,11 +72,11 @@ public class RobotContainer {
 
     public void stop() {
         m_Drivetrain.disabledInit();
-        m_Arm.stop();
+        /*m_Arm.stop();
         m_Intake.stop();
         m_Manipulator.stop();
         m_Shooter.stop();
-        m_Climber.stop();
+        m_Climber.stop();*/
     }
 
     private void configureBindings() {
@@ -87,7 +87,7 @@ public class RobotContainer {
         _driverController.share().onTrue(
                 new InstantCommand(m_Drivetrain::resetYaw)); // toggle field relative mode
 
-        m_buttonController.L1().onTrue(getCollectSequence());
+        /*m_buttonController.L1().onTrue(getCollectSequence());
         m_buttonController.R1().onTrue(getShootSequence());
         m_buttonController.cross().whileTrue(getAMPSequence());
         m_buttonController.square().onTrue(m_Arm.getSetStateCommand(ArmState.UNDER_CHAIN));
@@ -96,12 +96,12 @@ public class RobotContainer {
          * driver should press this before cross to save time, but cross still includes
          * arm to amp in case of mistake
          */
-        m_buttonController.circle().onTrue(m_Arm.getSetStateCommand(ArmState.AMP));
+        //m_buttonController.circle().onTrue(m_Arm.getSetStateCommand(ArmState.AMP));
 
         _driverController.cross().onTrue(m_SysidCommands.fullSysidRun());
     }
 
-    private Command getCollectSequence() {
+    /*private Command getCollectSequence() {
         return Commands.sequence(
                 m_Arm.getSetStateCommand(ArmState.COLLECT)
                         .alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.COLLECT)),
@@ -140,7 +140,7 @@ public class RobotContainer {
 
     private Command getClimbSequence() {
         return m_Arm.getSetStateCommand(ArmState.TRAP).andThen(m_Climber.getClimbCommand());
-    }
+    }*/
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
