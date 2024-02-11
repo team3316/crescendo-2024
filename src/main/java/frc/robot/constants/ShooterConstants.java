547f392b8fc77d7f2231802a907af72ebbb5709c
itamrit77 <itamrit77@gmail.com>
2024-02-11 14:28:42 +0200

    moved shooter kf & kp to percentage units (need additional calibration)

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -11,8 +11,8 @@ public class ShooterConstants {
     public static final double SparkFlexUnShootingVelocity = 0; // rpm
     public static final double SparkFlexShootingVelocity = 1; // rpm
 
-    public static final double kpShooter = 1; // v/rpm
-    public static final double kfShooter = 0.00178571428; // (1/560)v/rpm 
+    public static final double kpShooter = 0; // percentage/rpm
+    public static final double kfShooter = (1 / 12.7) / 550; // percentage/rpm 
 
     public static final double shootingTime = 2; // seconds
 }
