b3de1046e24338528c1ff1f9ca03585f7552ba43
yoavpeles <yoav.peles@gmail.com>
2024-02-13 20:21:17 +0200

    added correct shooter constants & units

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -2,8 +2,8 @@ package frc.robot.constants;
 
 
 public class ShooterConstants {
-    public static final int leaderUpLeftPort = 14;
-    public static final int followerUpRightPort = 15;
+    public static final int leaderUpLeftPort = 12;
+    public static final int followerUpRightPort = 13;
     public static final int followerDownLeftPort = 12;
     public static final int followerDownRightPort = 13;
 
@@ -11,7 +11,11 @@ public class ShooterConstants {
     public static final double SparkFlexUnShootingVelocity = 0; // rpm
     public static final double SparkFlexShootingVelocity = 1; // rpm
 
-    public static final double kpShooter = 0; // percentage/rpm
-    public static final double kfShooter = 0.75 / 5090; // percentage/rpm 
+    private static final double wheelDiameter = 4 * 0.0254; // 4 inches in meters
+    public static final double positionFactor = wheelDiameter; // meters / rotation
+    public static final double velocityFactor = wheelDiameter / 60; // (meters / second) / RPM
+
+    public static final double kpShooter = 0.00005 / velocityFactor; // percentage/rpm
+    public static final double kfShooter = 0.75 / (4874 * velocityFactor); // percentage/(meters per second)
     public static final double shootingTime = 2; // seconds
 }
