5405c70342bc82a653bf17406c9dce6472288e38
yoavpeles <yoav.peles@gmail.com>
2024-02-16 15:22:38 +0200

    removed second manipulator motor

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -10,8 +10,7 @@ import frc.robot.motors.DBugSparkMax;
 
 public class Manipulator extends SubsystemBase {
 
-    private DBugSparkMax _leader;
-    private DBugSparkMax _follower;
+    private DBugSparkMax _manipulatorMotor;
 
     private DigitalInput _hasNoteSwitch;
 
@@ -34,10 +33,8 @@ public class Manipulator extends SubsystemBase {
 
     public Manipulator() {
         // TODO: check what ports it should be
-        this._leader = DBugSparkMax.create(ManipulatorConstants.lowerSparkMaxPort);
-        this._follower = DBugSparkMax.create(ManipulatorConstants.upperSparkMaxPort);
-        this._leader.setSmartCurrentLimit(20);
-        this._follower.setSmartCurrentLimit(20);
+        this._manipulatorMotor = DBugSparkMax.create(ManipulatorConstants.lowerSparkMaxPort);
+        this._manipulatorMotor.setSmartCurrentLimit(20);
         
         this._hasNoteSwitch = new DigitalInput(ManipulatorConstants.noteSwitchPort);
 
@@ -62,11 +59,9 @@ public class Manipulator extends SubsystemBase {
     private void setState(ManipulatorState state) {
         this._state = state;
 
-        this._leader.set(state.leftPercentage);
-        this._follower.set(state.rightPercentage);
+        this._manipulatorMotor.set(state.leftPercentage);
 
         SmartDashboard.putString("Manipulator State", this._state.toString());
-      
     }
 
     public Command getSetStateCommand(ManipulatorState state) {
@@ -77,29 +72,9 @@ public class Manipulator extends SubsystemBase {
         setState(ManipulatorState.OFF);
     }
 
-    public void manipulatByPercent(double follower,double leader){
-        this._leader.set(leader);
-        this._follower.set(follower);
-    }
-
     @Override
     public void periodic() {
-        // ManipulatorState.COLLECT.rightPercentage = SmartDashboard.getNumber("manipulator collect right", 0);
-        // ManipulatorState.COLLECT.leftPercentage = SmartDashboard.getNumber("manipulator collect left", 0);
-        // ManipulatorState.TO_SHOOTER.rightPercentage = SmartDashboard.getNumber("manipulator to shooter right", 0);
-        // ManipulatorState.TO_SHOOTER.leftPercentage = SmartDashboard.getNumber("manipulator to shooter left", 0);
-        //SmartDashboard.putNumber("manipulator collect", SmartDashboard.getNumber("manipulator collect", 0));
-        //SmartDashboard.putNumber("manipulator to shooter", SmartDashboard.getNumber("manipulator to shooter", 0));
         SmartDashboard.putBoolean("has note", hasNoteSwitch());
-        // SmartDashboard.putNumber("left current", _leader.getOutputCurrent());
-        // SmartDashboard.putNumber("right current", _follower.getOutputCurrent());
-        SmartDashboard.putNumber("right velocity rpm", _follower.getVelocity());
-        SmartDashboard.putNumber("left velocity rpm", _leader.getVelocity());
-        // SmartDashboard.putNumber("manipulator collect right", SmartDashboard.getNumber("manipulator collect right", 0));
-        // SmartDashboard.putNumber("manipulator collect left", SmartDashboard.getNumber("manipulator collect left", 0));
-        // SmartDashboard.putNumber("manipulator to shooter right", SmartDashboard.getNumber("manipulator to shooter right", 0));
-        // SmartDashboard.putNumber("manipulator to shooter left", SmartDashboard.getNumber("manipulator to shooter left", 0));
-
-     //   manipulatByPercent(SmartDashboard.getNumber("Manipulator Percentage leader", 0), SmartDashboard.getNumber("Manipulator Percentage follower", 0));
+        SmartDashboard.putNumber("manipulator velocity rpm", _manipulatorMotor.getVelocity());
     }
 }
