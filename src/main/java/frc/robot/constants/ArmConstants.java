f1bda4d33d4b9c183edf40cfb5a07b14a92ee0e4
itamrit77 <itamrit77@gmail.com>
2024-02-16 15:45:23 +0200

    added second joint

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -5,25 +5,39 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 public class ArmConstants {
     public static final int leaderCANID = 0;
     public static final int followerCANID = 0;
+    public static final int jointCANID = 0;
 
     public static final double collectAngle = 0; // deg
     public static final double TRAPAngle = 0; // deg
     public static final double underChainAngle = 0; // deg
     public static final double AMPAngle = 0; // deg
+    public static final double collectManipulatorAngle = 0;
+    public static final double AMPManipulatorAngle = 0;
+    public static final double TRAPManipulatorAngle = 0;
+    public static final double underChainManipulatorAngle = 0;
 
-    private static final double maxVelocity = 0; // deg/sec
-    private static final double maxAcceleration = 0; // deg/sec^2
-    public static final TrapezoidProfile.Constraints profileConstrains = new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
+    private static final double maxArmVelocity = 0; // deg/sec
+    private static final double maxArmAcceleration = 0; // deg/sec^2
+    public static final TrapezoidProfile.Constraints armProfileConstrains = new TrapezoidProfile.Constraints(maxArmVelocity, maxArmAcceleration);
+    private static final double maxMnaipulatorJointVelocity = 0; // deg/sec
+    private static final double maxManipulatorJointAcceleration = 0; // deg/sec^2
+    public static final TrapezoidProfile.Constraints manipulatorJointProfileConstrains = new TrapezoidProfile.Constraints(maxMnaipulatorJointVelocity, maxManipulatorJointAcceleration);
 
-    public static final double ks = 0; // v
-    public static final double kg = 0; // v
-    public static final double kv = 0; // v/(rad/sec)
-    public static final double ka = 0; // v/(rad/sec^2)
+    public static final double armKs = 0; // v
+    public static final double armKg = 0; // v
+    public static final double armKv = 0; // v/(rad/sec)
+    public static final double armKa = 0; // v/(rad/sec^2)
+    public static final double manipulatorJointKg = 0; // v
+    public static final double manipulatorJointKv = 0; // v/(rad/sec)
 
-    public static final double kp = 0;
+    public static final double armKp = 0;
+    public static final double manipulatorJointKp = 0;
 
-    private static final double gearRatio = 1.0 / 64.0;
-    public static final double positionFactor = 360 * gearRatio; // deg/rotations
-    public static final double velocityFactor = 360 * gearRatio / 60; // (deg/sec)/RPM
+    private static final double armGearRatio = 1.0 / 64.0;
+    public static final double armPositionFactor = 360 * armGearRatio; // deg/rotations
+    public static final double armVelocityFactor = 360 * armGearRatio / 60; // (deg/sec)/RPM
+    private static final double manipulatorJointGearRatio = 1.0 / 64.0;
+    public static final double manipulatorJointPositionFactor = 360 * manipulatorJointGearRatio; // deg/rotations
+
 
 }
--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -2,6 +2,13 @@ package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.compound.Diff_PositionVoltage_Velocity;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.ReverseLimitValue;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.SoftLimitDirection;
 import com.revrobotics.SparkLimitSwitch.Type;
@@ -13,6 +20,7 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
@@ -24,41 +32,52 @@ public class Arm extends SubsystemBase {
 
     private DBugSparkMax _leader;
     private DBugSparkMax _follower;
+    private TalonFX _manipulatorJoint;
 
-    private ArmFeedforward _feedforward;
+    private ArmFeedforward _armFeedforward;
 
     private ArmState _targetState;
 
     public static enum ArmState {
-        COLLECT(ArmConstants.collectAngle),
-        AMP(ArmConstants.AMPAngle),
-        UNDER_CHAIN(ArmConstants.underChainAngle),
-        TRAP(ArmConstants.TRAPAngle);
+        COLLECT(ArmConstants.collectAngle, ArmConstants.collectManipulatorAngle),
+        AMP(ArmConstants.AMPAngle, ArmConstants.AMPManipulatorAngle),
+        UNDER_CHAIN(ArmConstants.underChainAngle, ArmConstants.underChainManipulatorAngle),
+        TRAP(ArmConstants.TRAPAngle, ArmConstants.TRAPManipulatorAngle);
 
-        public final double angleDeg;
+        public final double armAngleDeg;
+        public final double manipulatorAngleDeg;
 
-        private ArmState(double angleDeg) {
-            this.angleDeg = angleDeg;
+        private ArmState(double angleDeg, double manipulatorAngleDeg) {
+            this.armAngleDeg = angleDeg;
+            this.manipulatorAngleDeg = manipulatorAngleDeg;
         }
     }
 
     public Arm() {
-        _leader = DBugSparkMax.create(ArmConstants.leaderCANID, new PIDFGains(ArmConstants.kp), 
-        ArmConstants.positionFactor, ArmConstants.velocityFactor, 0);
-        _follower = DBugSparkMax.create(ArmConstants.followerCANID, new PIDFGains(ArmConstants.kp), 
-        ArmConstants.positionFactor, ArmConstants.velocityFactor, 0);
+        _leader = DBugSparkMax.create(ArmConstants.leaderCANID, new PIDFGains(ArmConstants.armKp), 
+        ArmConstants.armPositionFactor, ArmConstants.armVelocityFactor, 0);
+        _follower = DBugSparkMax.create(ArmConstants.followerCANID, new PIDFGains(ArmConstants.armKp), 
+        ArmConstants.armPositionFactor, ArmConstants.armVelocityFactor, 0);
         _follower.follow(_leader, false); // TODO: verify inversion before testing
-        _leader.setSoftLimit(SoftLimitDirection.kForward, (float)ArmState.TRAP.angleDeg + 2); // TODO: check which side (fwd/rev) is soft and which is hard limit
+        _leader.setSoftLimit(SoftLimitDirection.kForward, (float)ArmState.TRAP.armAngleDeg + 2); // TODO: check which side (fwd/rev) is soft and which is hard limit
         _leader.enableSoftLimit(SoftLimitDirection.kForward, true);
+        
+        _manipulatorJoint = new TalonFX(ArmConstants.jointCANID);
+        TalonFXConfiguration jointConfig = new TalonFXConfiguration();
+        jointConfig.MotorOutput.withNeutralMode(NeutralModeValue.Brake);
+        jointConfig.Slot0.withKP(ArmConstants.manipulatorJointKp).withKV(ArmConstants.manipulatorJointKv).
+        withKG(ArmConstants.armKg);
+        jointConfig.Feedback.withSensorToMechanismRatio(1 / ArmConstants.manipulatorJointPositionFactor);
+        _manipulatorJoint.getConfigurator().apply(jointConfig);
 
-        _feedforward = new ArmFeedforward(ArmConstants.ks, ArmConstants.kg, ArmConstants.kv, ArmConstants.ka);
-
+        _armFeedforward = new ArmFeedforward(ArmConstants.armKs, ArmConstants.armKg, ArmConstants.armKv, ArmConstants.armKa);
+        
         _targetState = getInitialState();
-        _leader.setPosition(_targetState.angleDeg);
+        _leader.setPosition(_targetState.armAngleDeg);
     }
 
     private ArmState getInitialState() {
-        if (isRevLimitSwitchClosed()) {
+        if (isArmRevLimitSwitchClosed()) {
             return ArmState.COLLECT;
         }
         return ArmState.TRAP;
@@ -69,52 +88,107 @@ public class Arm extends SubsystemBase {
     }
 
     // TODO: check if switches are NC or NO
-    private boolean isRevLimitSwitchClosed() {
+    public boolean isArmRevLimitSwitchClosed() {
         return _leader.getReverseLimitSwitch(Type.kNormallyOpen).isPressed();
     }
 
-    public double getPositionDeg() {
+    public boolean isManipulatorFwdLimitSwitchClosed() {
+        return _manipulatorJoint.getForwardLimit().getValue() == ForwardLimitValue.ClosedToGround;
+    }
+
+    public boolean isManipulatorRevLimitSwitchClosed() {
+        return _manipulatorJoint.getReverseLimit().getValue() == ReverseLimitValue.ClosedToGround;
+    }
+
+    public double getArmPositionDeg() {
         return _leader.getPosition();
     }
 
-    public double getVelocityDegPerSec() {
+    public double getArmVelocityDegPerSec() {
         return _leader.getVelocity();
     }
 
-    private State getCurrentTrapezoidState() {
-        return new State(getPositionDeg(), getVelocityDegPerSec());
+    private State getCurrentArmTrapezoidState() {
+        return new State(getArmPositionDeg(), getArmVelocityDegPerSec());
     }
 
-    private void useState(TrapezoidProfile.State targetState) {
-        double feedforward = _feedforward.calculate(Math.toRadians(targetState.position), Math.toRadians(targetState.velocity));
+    public double getMnaipulatorJointPositionDeg() {
+        return _manipulatorJoint.getPosition().getValueAsDouble();
+    }
+
+    public double getManipulatorJointVelocityDegPerSec() {
+        return _manipulatorJoint.getPosition().getValueAsDouble();
+    }
+
+    private State getManipulatorJointTrapezoidState() {
+        return new State(getMnaipulatorJointPositionDeg(), getManipulatorJointVelocityDegPerSec());
+    }
+
+    private void useArmState(TrapezoidProfile.State targetState) {
+        double feedforward = _armFeedforward.calculate(Math.toRadians(targetState.position), Math.toRadians(targetState.velocity));
         _leader.setReference(targetState.position, ControlType.kPosition, 0, feedforward, ArbFFUnits.kVoltage);
         SmartDashboard.putNumber("target arm position (deg)", targetState.position);
         SmartDashboard.putNumber("target arm velocity (deg/sec)", targetState.velocity);
     }
 
-    public Command getSetStateCommand(ArmState targetState) {
-        TrapezoidProfile profile = new TrapezoidProfile(ArmConstants.profileConstrains);
-        Supplier<State> targetSupplier = () -> (new State(targetState.angleDeg, 0));
-        return new InstantCommand(() -> {_targetState = targetState;}).alongWith(
-            new TrapezoidProfileCommand(profile, this::useState, targetSupplier, this::getCurrentTrapezoidState, this));
+    private Command getSetArmStateCommand(ArmState targetState) {
+        TrapezoidProfile profile = new TrapezoidProfile(ArmConstants.armProfileConstrains);
+        Supplier<State> targetSupplier = () -> (new State(targetState.armAngleDeg, 0));
+        return new TrapezoidProfileCommand(profile, this::useArmState, targetSupplier, this::getCurrentArmTrapezoidState, this);
         // no need for dynamic command as the new TrapezoidProfileCommand gets the start and goal states as Suppliers
+    }
+
+    private void useManipulatorJointState(TrapezoidProfile.State targetState) {
+        _manipulatorJoint.setControl(new PositionVoltage(
+            targetState.position, targetState.velocity, false, 0, 0, false, true, true));
+        SmartDashboard.putNumber("target manipulator joint position (deg)", targetState.position);
+        SmartDashboard.putNumber("target manipulator joint velocity (deg/sec)", targetState.velocity);
+    }
+
+    private Command getSetManipulatorJointStateCommand(ArmState targetState) {
+        TrapezoidProfile profile = new TrapezoidProfile(ArmConstants.manipulatorJointProfileConstrains);
+        Supplier<State> targetSupplier = () -> (new State(targetState.manipulatorAngleDeg, 0));
+        return new TrapezoidProfileCommand(profile, this::useManipulatorJointState, targetSupplier, this::getManipulatorJointTrapezoidState, this);
+    }
+
+    public Command getSetStateCommand(ArmState targetState) {
+        Command toReturn = Commands.parallel(
+            getSetArmStateCommand(targetState),
+            getSetManipulatorJointStateCommand(targetState),
+            new InstantCommand(() -> {_targetState = targetState;})
+        );
+        toReturn.addRequirements(this);
+        return toReturn;
     }
 
     public void stop() {
         _leader.set(0);
+        _manipulatorJoint.set(0);
     }
 
     private void updateSDB() {
         SmartDashboard.putString("target arm state", _targetState.toString());
-        SmartDashboard.putNumber("current arm position (deg)", getPositionDeg());
-        SmartDashboard.putNumber("current arm velocity (deg/s)", getVelocityDegPerSec());
-        SmartDashboard.putBoolean("arm rev limit", isRevLimitSwitchClosed());
+        SmartDashboard.putNumber("current arm position (deg)", getArmPositionDeg());
+        SmartDashboard.putNumber("current arm velocity (deg/s)", getArmVelocityDegPerSec());
+        SmartDashboard.putNumber("current manipulator joint position", getMnaipulatorJointPositionDeg());
+        SmartDashboard.putNumber("current manipulator joint velocity", getManipulatorJointVelocityDegPerSec());
+        SmartDashboard.putBoolean("arm rev limit", isArmRevLimitSwitchClosed());
+        SmartDashboard.putBoolean("manipulator fwd limit", isManipulatorFwdLimitSwitchClosed());
+        SmartDashboard.putBoolean("manipulator rev limit", isManipulatorRevLimitSwitchClosed());
     }
 
     @Override
     public void periodic() {
-        if (DriverStation.isDisabled() && isRevLimitSwitchClosed()) {
-            _leader.setPosition(ArmState.COLLECT.angleDeg);
+        if (DriverStation.isDisabled()) {
+            if (isArmRevLimitSwitchClosed()) {
+                _leader.setPosition(ArmState.COLLECT.armAngleDeg);
+            }
+            if (isManipulatorRevLimitSwitchClosed()) {
+                _manipulatorJoint.setPosition(ArmState.COLLECT.manipulatorAngleDeg);
+            }
+            else if (isManipulatorFwdLimitSwitchClosed()) {
+                _manipulatorJoint.setPosition(ArmState.TRAP.manipulatorAngleDeg);
+            }
         }
         updateSDB();
     }
