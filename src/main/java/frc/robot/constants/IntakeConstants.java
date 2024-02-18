db7fb19f5889dc7f2fc796186ddd265cdfee693c
yoavpeles <yoav.peles@gmail.com>
2024-02-18 23:46:25 +0200

    added intake switch & increased percentage

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -3,7 +3,10 @@ package frc.robot.constants;
 public class IntakeConstants {
     public final static int intakeMotorID = 11;
 
-    public final static double collectingPercentage = 0.75;
+    public final static double collectingPercentage = 1;
     public final static double ejectPercentage = -0.4;
     public final static double disabledPrecent = 0;
+        public final static int sensor_port = 3;
+
+
 }
