021bc98d71b0f0782d2d31f91dc3bfe6bb729259
yoavpeles <yoav.peles@gmail.com>
2024-02-21 15:28:44 +0200

    increased intake eject percentage

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -4,7 +4,7 @@ public class IntakeConstants {
     public final static int intakeMotorID = 11;
 
     public final static double collectingPercentage = 1;
-    public final static double ejectPercentage = -0.4;
+    public final static double ejectPercentage = -0.8;
     public final static double disabledPrecent = 0;
     public final static int sensor_port = 5;
 
