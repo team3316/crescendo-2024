b224218e3776d28c1cce13458c1f569100a67492
Barak Shelef <barak@oasis.security>
2024-02-22 09:38:21 +0200

    cleanup limelight

--- src/main/java/frc/robot/subsystems/vision/LimeLight.java
+++ src/main/java/frc/robot/subsystems/vision/LimeLight.java
@@ -13,32 +13,23 @@ import frc.robot.constants.LimelightConstants;
 
 public class LimeLight extends SubsystemBase {
     NetworkTable limeLightTable;
-    private NetworkTableEntry ta;
     private NetworkTableEntry tx;
-    private NetworkTableEntry ty;
     private NetworkTableEntry hasTarget;
     private NetworkTableEntry pipeLine;
     private NetworkTableEntry LEDs;
-    private double hDiff = 0;
 
     /** Creates a new LimeLight. */
     public LimeLight() { // CR: add a way to send the config to the limelight trough code
-   
+
         limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
-        ta = limeLightTable.getEntry("ta");
         tx = limeLightTable.getEntry("tx");
-        ty = limeLightTable.getEntry("ty");
         hasTarget = limeLightTable.getEntry("tv");
         pipeLine = limeLightTable.getEntry("pipeline");
         LEDs = limeLightTable.getEntry("ledMode");
         SmartDashboard.putNumber("tx", tx.getDouble(52));
-             setPipeLine(0);
+        setPipeLine(0);
     }
-    public double getArea(){
-  
-        return this.ta.getDouble(-1);
-        
-    }
+
     public boolean hasTarget() {
         return hasTarget.getDouble(0) == 1;
     }
@@ -47,38 +38,18 @@ public class LimeLight extends SubsystemBase {
         return LimelightConstants.AlignLimeLightToPigeonPhase * tx.getDouble(0);
     }
 
-    private double getYAngle() {
-        return ty.getDouble(0);
-    }
-
-    public double getFieldYMeters() {
-        return this.hDiff * Math.sin(Math.toRadians(this.getXAngle()))
-                / Math.tan(Math.toRadians(this.getYAngle()));
-    }
-
-    public double getFieldXMeters() {
-        return this.hDiff * Math.cos(Math.toRadians(this.getXAngle()))
-                / Math.tan(Math.toRadians(this.getYAngle()));
-    }
-
     public void setPipeLine(double id) {
         pipeLine.setNumber(id);
-    }
-
-    public void setTargetHeightDiff(double hDiff) {
-        this.hDiff = hDiff;
     }
 
     public void forceLEDsOff(boolean off) {
         LEDs.setNumber(off ? LimelightConstants.LEDsForceOff : LimelightConstants.LEDsByPipeline);
     }
 
-
     @Override
     public void periodic() {
-        SmartDashboard.putBoolean("hasTarget",hasTarget() );
-
-        SmartDashboard.putNumber("xLength", this.getFieldXMeters());
-        SmartDashboard.putNumber("yLength", this.getFieldYMeters());
+        SmartDashboard.putBoolean("hasTarget", hasTarget());
+        if (hasTarget())
+            SmartDashboard.putNumber("getXAngle", getXAngle());
     }
 }
