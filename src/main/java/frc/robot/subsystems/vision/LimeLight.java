28830287049f7e9edf84a046a9d0306de6b065f8
Barak Shelef <barak@oasis.security>
2024-02-22 12:02:31 +0200

    add LimelightHelpers from LimelightVision/limelightlib-wpijava

--- src/main/java/frc/robot/subsystems/vision/LimeLight.java
+++ src/main/java/frc/robot/subsystems/vision/LimeLight.java
@@ -4,46 +4,22 @@
 
 package frc.robot.subsystems.vision;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.LimelightConstants;
 
 public class LimeLight extends SubsystemBase {
-    NetworkTable limeLightTable;
-    private NetworkTableEntry tx;
-    private NetworkTableEntry hasTarget;
-    private NetworkTableEntry pipeLine;
-    private NetworkTableEntry LEDs;
-
     /** Creates a new LimeLight. */
     public LimeLight() { // CR: add a way to send the config to the limelight trough code
-
-        limeLightTable = NetworkTableInstance.getDefault().getTable("limelight");
-        tx = limeLightTable.getEntry("tx");
-        hasTarget = limeLightTable.getEntry("tv");
-        pipeLine = limeLightTable.getEntry("pipeline");
-        LEDs = limeLightTable.getEntry("ledMode");
-        SmartDashboard.putNumber("tx", tx.getDouble(52));
-        setPipeLine(0);
+        LimelightHelpers.setPipelineIndex(null, 0);
     }
 
     public boolean hasTarget() {
-        return hasTarget.getDouble(0) == 1;
+        return LimelightHelpers.getTV(null);
     }
 
     public double getXAngle() {
-        return LimelightConstants.AlignLimeLightToPigeonPhase * tx.getDouble(0);
-    }
-
-    public void setPipeLine(double id) {
-        pipeLine.setNumber(id);
-    }
-
-    public void forceLEDsOff(boolean off) {
-        LEDs.setNumber(off ? LimelightConstants.LEDsForceOff : LimelightConstants.LEDsByPipeline);
+        return LimelightConstants.AlignLimeLightToPigeonPhase * LimelightHelpers.getTX(null);
     }
 
     @Override
