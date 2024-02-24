a537edebe0fecc5e8154bfec397fc66a1a967417
UriAvidan <aavidan.uri@gmail.com>
2024-02-24 19:40:09 +0200

    dont flip path

--- src/main/java/frc/robot/autonomous/AutoFactory.java
+++ src/main/java/frc/robot/autonomous/AutoFactory.java
@@ -32,7 +32,7 @@ public class AutoFactory {
             0.4, // Drive base radius in meters. Distance from robot center to furthest module.
             new ReplanningConfig() // Default path replanning config. See the API for the options here
         ),
-        drivetrain :: shouldFlipPath,
+        (()-> false),
         drivetrain // Reference to this subsystem to set requirements
         );
     }
