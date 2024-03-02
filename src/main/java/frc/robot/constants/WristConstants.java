f41a0639849068d2f10265f084db8729a541048f
itamrit77 <itamrit77@gmail.com>
2024-03-02 17:34:38 +0200

    setted wrist amp angle and increased velocity & acceleration

--- src/main/java/frc/robot/constants/WristConstants.java
+++ src/main/java/frc/robot/constants/WristConstants.java
@@ -6,17 +6,16 @@ public class WristConstants {
     public static final int wristCANID = 9;
     
     public static final double collectAngle = -110.5; // deg
-    public static final double AMPAngle = -34.68; // deg
+    public static final double AMPAngle = 0; // deg
     public static final double TRAPAngle = -30.68; // deg
     public static final double underChainAngle = -110.5; // deg
 
     private static final double gearRatio = 1.0 / 35.0;
     public static final double positionFactor = 360 * gearRatio; // deg/rotations
 
-    private static final double maxVelocity = 20; // deg/sec
-    private static final double maxAcceleration = 20; // deg/sec^2
+    private static final double maxVelocity = 300; // deg/sec
+    private static final double maxAcceleration = 300; // deg/sec^2
     public static final TrapezoidProfile.Constraints profileConstrains = new TrapezoidProfile.Constraints(maxVelocity, maxAcceleration);
-
     public static final double kg = 0.15; // v
     public static final double kv = 3 / (Math.PI * 275 / 180); // v/(rad/sec)
 
