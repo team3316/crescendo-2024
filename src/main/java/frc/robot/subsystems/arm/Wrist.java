924dd12b6542e96289a33472fb9eb37c73515551
itamrit77 <itamrit77@gmail.com>
2024-03-03 18:37:50 +0200

    setted update sdb to false

--- src/main/java/frc/robot/subsystems/arm/Wrist.java
+++ src/main/java/frc/robot/subsystems/arm/Wrist.java
@@ -26,7 +26,7 @@ import frc.robot.subsystems.arm.ArmWristSuperStructure.ArmWristState;
 
 public class Wrist extends SubsystemBase {
 
-    private static final boolean UPDATE_DASHBOARD = true;
+    private static final boolean UPDATE_DASHBOARD = false;
 
     private TalonFX _wristMotor;
 
