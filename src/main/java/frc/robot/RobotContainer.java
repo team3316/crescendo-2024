fe1fb2f7be7c31941010477b6a69253b5a3fce32
UriAvidan <aavidan.uri@gmail.com>
2024-03-05 11:21:03 +0200

    format

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -111,11 +111,15 @@ public class RobotContainer {
         m_operatorController.R1().onTrue(getShooterTriggerCommand());
         m_operatorController.R2().whileTrue(getShooterSpinCommand());
 
-        m_operatorController.povUp().whileTrue(new StartEndCommand(()->m_Intake.setStateCommand(IntakeState.EJECT).alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.EJECT)),()->m_Intake.setStateCommand(IntakeState.DISABLED).alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.OFF))));
-
-
+        m_operatorController.povUp()
+                .whileTrue(new StartEndCommand(
+                        () -> m_Intake.setStateCommand(IntakeState.EJECT)
+                                .alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.EJECT)),
+                        () -> m_Intake.setStateCommand(IntakeState.DISABLED)
+                                .alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.OFF))));
                 
-                m_operatorController.cross().onTrue(m_Intake.setStateCommand(IntakeState.DISABLED).alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.OFF)));
+        m_operatorController.cross().onTrue(m_Intake.setStateCommand(IntakeState.DISABLED)
+                .alongWith(m_Manipulator.getSetStateCommand(ManipulatorState.OFF)));
         m_operatorController.circle()
                 .onTrue(Commands.sequence(m_Manipulator.getSetStateCommand(ManipulatorState.AMP),
                         new WaitCommand(3), m_Manipulator
