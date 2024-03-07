0cc888eb04fdfaf7d28a6ed9200d0503ea2b5699
UriAvidan <aavidan.uri@gmail.com>
2024-03-07 18:42:55 +0200

    updating sdb

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends SubsystemBase {
     
-    private static final boolean UPDATE_DASHBOARD = false;
+    private static final boolean UPDATE_DASHBOARD = true;
 
     private DBugSparkFlex _leaderLeft;
     private DBugSparkFlex _followerRight;
