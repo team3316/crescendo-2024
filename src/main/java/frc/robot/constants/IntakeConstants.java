2a9b15622db33a51cf9f21981b6246985256ec24
UriAvidan <aavidan.uri@gmail.com>
2024-03-09 11:29:19 +0200

    added constants for intake pid control

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.constants;
 
+import frc.robot.motors.PIDFGains;
+
 public class IntakeConstants {
     public final static int intakeMotorID = 11;
     public static final int rollerID = 6;
@@ -8,5 +10,14 @@ public class IntakeConstants {
     public final static double collectingPercentage = 1;
     public final static double ejectPercentage = -0.8;
     public final static double disabledPrecent = 0;
+
+    public final static double intakeKp = 0;
+    public final static double rollerkp = 0;
+
+    public final static PIDFGains intakeGains = new PIDFGains(intakeKp, 0, 0, 0);
+    public final static PIDFGains rollerGains = new PIDFGains(rollerkp, 0, 0, 0);
+
+    public final static double intakeVelocityFactor = 0;//m/s
+    public final static double rollerVelocityFactor = 0;//m/s
 
 }
