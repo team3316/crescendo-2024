10322df4209cacd73f4cee423d2d29724be03c41
itamrit77 <itamrit77@gmail.com>
2024-03-12 19:49:49 +0200

    removed unused variables and commented lines

--- src/main/java/frc/robot/subsystems/arm/ArmWristSuperStructure.java
+++ src/main/java/frc/robot/subsystems/arm/ArmWristSuperStructure.java
@@ -11,17 +11,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.ArmConstants;
 import frc.robot.constants.WristConstants;
-import frc.robot.utils.LatchedBoolean;
 
 public class ArmWristSuperStructure extends SubsystemBase {
 
     private final Arm m_Arm;
     private final Wrist m_Wrist;
-    // TODO: verify rising or falling edge (depends on NC/NO)
     private final Debouncer _recalibrationDebouncer = new Debouncer(2);
     private DigitalInput _coastSwitch = new DigitalInput(ArmConstants.coastSwitchPort);
-    private LatchedBoolean _shouldChangeMode = new LatchedBoolean();
-    private boolean _isBreakMode = true;
     private Trigger _toggleNutoralMode;
 
     public ArmWristSuperStructure() {
@@ -76,21 +72,6 @@ public class ArmWristSuperStructure extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // if (DriverStation.isDisabled()) {
-        //     // only update once per button press - software latch
-        //     if (_shouldChangeMode.update(!_coastSwitch.get())) {
-        //         // flip the currently latched state
-        //         _isBreakMode = !_isBreakMode;
-        //         setBreakMode(_isBreakMode);
-        //     }
-        // } else {
-        //     // set break mode once if it was false
-        //     if (!_isBreakMode) {
-        //             _isBreakMode = true;
-        //             setBreakMode(_isBreakMode);
-        //     }
-        // }
-
         if (_recalibrationDebouncer.calculate(m_Arm.anyLimitSwitchClosed())) {
             setEncodersToCollect();
         }
